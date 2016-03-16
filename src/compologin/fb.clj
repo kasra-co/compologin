dbabf001ae9606d2b0c42f7987cd97c883442f7f
Dan Ross <ross9885@gmail.com>
2016-03-17 02:16:24 +0300

    Cleanup, correct incorrectly nested credentials

--- src/compologin/fb.clj
+++ src/compologin/fb.clj
@@ -1,15 +1,15 @@
 (ns compologin.fb
   (:require [clj-http.client :as client]
-             [clojure.data.json :as json]
-             [ring.util.codec :refer [form-encode]]))
+            [clojure.data.json :as json]
+            [ring.util.codec :refer [form-encode]]))
 
 (let [fb-graph-api "https://graph.facebook.com/v2.5"]
 
   (defn request-app-token
     "Request an app token from FB. Useful for app-global actions, such as creating test users."
     [client-credentials]
-    (->> {"client_id" (client-credentials :client-id)
-          "client_secret" (client-credentials :client-secret)
+    (->> {"client_id" (:client-id client-credentials)
+          "client_secret" (:client-secret client-credentials)
           "grant_type" "client_credentials"}
          (form-encode)
          (str fb-graph-api "/oauth/access_token?")
@@ -21,10 +21,9 @@
   (defn request-access-token
     "Request an access token for an OAuth2 code. Useful for user-specific actions. `oauth-callback-url` must match the callback URL that was used to receive the OAuth2 code."
     [oauth-callback-url client-credentials oauth-code]
-    (-> {:client_id (client-credentials :client-id)
-         :redirect_uri oauth-callback-url
-         :client_secret {:client_id (:client-id client-credentials)
-                         :client_secret (:client-secret client-credentials)}
+    (-> {"client_id" (:client-id client-credentials)
+         "client_secret" (:client-secret client-credentials)
+         "redirect_uri" oauth-callback-url
          :code oauth-code}
         (form-encode)
         ((partial str fb-graph-api "/oauth/access_token?"))
@@ -50,10 +49,10 @@
   (defn request-token-info
     "Fetch basic information about any kind of graph API token, and its context."
     [client-credentials client-token subject-token]
-    (-> {:input_token subject-token
-          :access_token client-token}
-         (form-encode)
-         ((partial str "https://graph.facebook.com/debug_token?"))
-         (client/get {:throw-entire-message? true})
-         :body
-         (json/read-str))))
+    (-> {"input_token" subject-token
+         "access_token" client-token}
+        (form-encode)
+        ((partial str "https://graph.facebook.com/debug_token?"))
+        (client/get {:throw-entire-message? true})
+        :body
+        (json/read-str))))
